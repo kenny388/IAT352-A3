44b6faf6a8293f816d641d6ee6e5befcde31e299
Unknown <kennycheung388@yahoo.com.hk>
2018-10-15 19:15:29 -0700

    Created dbquery.php

---
+++ dbquery.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Database Query</title>
+    <link rel="stylesheet" href="css/dbquery.css">
+  </head>
+  <body>
+    <!-- page content -->
+
+    <div class="container">
+      <div class="row">
+        <h2>Query</h2>
+      </div>
+      <div class="row">
+        <div class="column">
+          <h3>Select Order Parameters</h3>
+        </div>
+        <div class="column">
+
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+</html>
